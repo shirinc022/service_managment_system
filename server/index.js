73f03339164d911b3c7e7f8d7c4f61911fc0a95f
SHIRIN <shirinc022@gmail.com>
2025-03-14 12:56:35 +0300

    backend welcome

--- server/index.js
+++ server/index.js
@@ -1,9 +1,11 @@
 const express=require('express')
+const cors =require('cors')
 require('dotenv').config()
 const connectDB=require('./src/config/db')
 const router = require('./src/routes/router')
 var cookieParser = require('cookie-parser')
-const cors =require('cors')
+
+
 
 
 
@@ -12,11 +14,13 @@ connectDB()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:process.env.FRONTEND_URL,
     credentials:true
 }))
 
-
+app.get('/',(req,res)=>{
+    res.send('welcome to my project')
+})
 app.use('/',router)
 
 
