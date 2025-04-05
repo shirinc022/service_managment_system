2a08d9e56ca9ce07bb5204ff0d6d689a497bdd08
SHIRIN <shirinc022@gmail.com>
2025-04-05 21:09:08 +0300

    required added in service model

--- server/src/models/serviceModel.js
+++ server/src/models/serviceModel.js
@@ -3,14 +3,13 @@ const mongoose = require('mongoose');
 
 const serviceSchema=new mongoose.Schema({
         provider_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Providers' ,required: true},
-        // reviews: [{ type: mongoose.Schema.Types.ObjectId, ref: "Reviews" }],
         title: { type: String, required: true},
         category: { type: String, required: true},
         description: { type: String, required: true },
         price: { type: Number, required: true },
         availability:{type:String,required: true},
         service_area:{type:String,required: true},
-        images:{type:[String]},
+        images:{type:[String],required: true},
     },{timestamps:true});
 
 
